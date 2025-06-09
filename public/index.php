f9d3f3faba171f246b4575ed442479ea2a53192d
filip12724 <bondzulicfilip@gmail.com>
2025-06-09 13:07:26 +0200

    Created a response class

--- public/index.php
+++ public/index.php
@@ -2,11 +2,17 @@
 declare(strict_types=1);
 
 use Elephant\Framework\Http\Request;
+use Elephant\Framework\Http\Response;
 
 define('BASE_PATH', dirname(__DIR__));
 
 require_once BASE_PATH . '/vendor/autoload.php';
 
 $request = Request::create();
-dd($request); 
-echo "Hello world";
+
+
+$content = "Hello world";
+
+$response = new Response($content);
+
+$response->send();
